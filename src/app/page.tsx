1404631df54f3548fbbc708eda5aaef733b37163
JairoPA <jairopreciadoayala@gmail.com>
2025-03-14 11:45:48 -0600

    prueba

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -95,7 +95,7 @@ export default function Home() {
             width={16}
             height={16}
           />
-          Go to nextjs.orga →
+          Go to nextjs.orgas →
         </a>
       </footer>
     </div>
