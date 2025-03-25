f4ea032b2d2993a9b008a116d22f0b260b256e6e
JairoPA <jairopreciadoayala@gmail.com>
2025-03-25 11:05:27 -0600

    a

---
+++ src/app/nosotros/page.tsx
@@ -0,0 +1,10 @@
+// app/page.tsx
+export default function HomePage() {
+    return (
+      <section>
+        <h2>Bienvenido a mi app Next.js</h2>
+        <p>Esta es la página principal.</p>
+      </section>
+    )
+  }
+  
