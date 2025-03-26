a67288edd272ef416267ca329ac0c628d71222b5
mittzifabiola <mrdonjuan@ucol.mx>
2025-03-26 17:36:29 -0600

    se creo page.tsx en about us

--- src/app/about-us/layout.tsx
+++ src/app/about-us/layout.tsx
@@ -1,30 +1,17 @@
-// app/layout.tsx
-import '../../styles/globals.css'
-import type { Metadata } from 'next'
+// src/app/(landing)/layout.tsx
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
+import "./../../styles/globals.css";
 
-export const metadata: Metadata = {
-  title: 'Mi App Next.js',
-  description: 'Una aplicación web con Next.js 13+',
-}
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+export default function LandingLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="es">
-      <body>
-        <header style={{ padding: '1rem', backgroundColor: '#eee' }}>
-          <h1>Mi App</h1>
-        </header>
-        <main style={{ padding: '1rem' }}>
-          {children}
-        </main>
-        <footer style={{ padding: '1rem', backgroundColor: '#eee' }}>
-          <p>© 2025 Mi Empresa</p>
-        </footer>
+      <body className="flex flex-col min-h-screen">
+        <Navbar />
+        <main className="flex-grow">{children}</main>
+        <Footer />
       </body>
     </html>
-  )
+  );
 }
