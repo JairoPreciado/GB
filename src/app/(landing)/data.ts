3cc1cd8d2d7421f70cb19c9d1fa908227ec21ffd
mittzifabiola <mrdonjuan@ucol.mx>
2025-04-09 16:09:17 -0600

    Se agrego la informacion de los servicios

---
+++ src/app/(landing)/data.ts
@@ -0,0 +1,37 @@
+export const servicios = [
+    {
+      titulo: "Reparación y Mantenimiento de Dispositivos Electrónicos",
+      descripcion:
+        "Ofrecemos servicios de reparación y mantenimiento para dispositivos electrónicos, asegurando su funcionamiento óptimo y prolongando su vida útil.",
+      lista: [
+        "Diagnóstico de dispositivos",
+        "Reparación de hardware",
+        "Mantenimiento preventivo",
+        "Actualización de software",
+      ],
+      imagen: "/assets/mantenimiento.jpg",
+    },
+    {
+      titulo: "Infraestructura y Redes",
+      descripcion:
+        "Ofrecemos diseño, instalación y soporte de redes cableadas e inalámbricas, asegurando un funcionamiento continuo, seguro y eficiente para hogares y negocios.",
+      lista: [
+        "Redes LAN, WiFi",
+        "Extensores de Red",
+        "Equipos de Punto a Punto",
+        "Cableado Estructurado"
+      ],
+      imagen: "/assets/redes.png",
+    },
+    {
+      titulo: "Desarrollo de Software y Venta de Equipos",
+      descripcion:
+        "Revisamos y reparamos impresoras para garantizar su funcionamiento continuo.",
+      lista: [
+        "Páginas Web",
+        "Sistemas de Punto de Venta para Restaurantes",
+        "Sistemas Personalizados",
+      ],
+      imagen: "/assets/software.jpg",
+    },
+  ];
